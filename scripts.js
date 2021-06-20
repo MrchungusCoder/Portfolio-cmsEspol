ddb95e15327265372338d92bb5dbc0471a13b106
MrchungusCoder <diegorr15@hotmail.com>
2021-06-20 14:05:22 -0500

    Portafolio casi finalizado, proximos cambios seran menores

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+"use strict";
 //////////////////////////////////////////////////////////// INTENTO DE MODO OSCURO
 
 // const darkbtn = document.querySelector(".btn");
@@ -31,14 +32,80 @@
 //     document.querySelector(".navar").style.left="-100%";
 // }
 
-const btnToggler = document.querySelector(".toggle_menu").addEventListener("click", deploy);
+// const btnToggler = document.querySelector(".toggle_menu").addEventListener("click", deploy);
 
-const closeButn = document.querySelector(".closer").addEventListener("click", close);
+// const closeButn = document.querySelector(".closer").addEventListener("click", close);
 
-function deploy() {
-  document.querySelector(".navar").style.left="100%"; 
-}
+// function deploy() {
+//   document.querySelector(".navar").style.left="100%"; 
+// }
 
-function close() {
-  document.querySelector(".navar").style.left="-100%";
+// function close() {
+//   document.querySelector(".navar").style.left="-100%";
+// }
+
+// document.querySelector(".toggle_menu").addEventListener("click", ()=> document.querySelector(".main_container--navbar").classList.toggle("show"));
+
+
+
+
+// const toggleBtn = document.querySelector(".toggle");
+// const closeBtn = document.querySelector(".close");
+
+// toggleBtn.addEventListener("click", function () {
+//   document.querySelector(".main_container--navbar").style.left="0";
+//   document.querySelector(".main_container--navbar").style.width="50%";
+
+// })
+
+// closeBtn.addEventListener("click",function(){
+//   document.querySelector(".main_container--navbar").style.left="-100%";
+//   // document.querySelector(".main_container--navbar").style.width="50%";
+ 
+// })
+
+
+
+// function myfunction(x) {
+//   if (x.matches) {
+//     const toggleBtn = document.querySelector(".toggle");
+// const closeBtn = document.querySelector(".close");
+
+//     toggleBtn.addEventListener("click", function () {
+//       document.querySelector(".main_container--navbar").style.left="0";
+//       document.querySelector(".main_container--navbar").style.width="50%";
+    
+//     })
+    
+//     closeBtn.addEventListener("click",function(){
+//       document.querySelector(".main_container--navbar").style.left="-100%";
+//       // document.querySelector(".main_container--navbar").style.width="50%";
+     
+//     })
+//   }
+// }
+
+// var x = window.matchMedia("(max-width: 900px)")
+// myfunction(x);
+// x.addEventListener(myfunction);
+  
+// window.onload=function(){
+//   const hamburguer = document.querySelector(".toogle");
+//   const navbar = document.querySelector(".main_container--navbar");
+  
+//   if(hamburguer) {
+//     hamburguer.addEventListener("click", function() {
+//     navbar.classList.toggle("open");
+//   })
+//   }
+// }
+
+
+const hamburguer = document.querySelector(".toogle");
+const navbar = document.querySelector(".main_container--navbar");
+
+if(hamburguer) {
+  hamburguer.addEventListener("click", function() {
+  navbar.classList.toggle("open");
+})
 }
